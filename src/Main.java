30fdd704abe8090a696626b933b23ddee35162d0
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-08-26 09:31:15 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long minutes){
+
+        if (minutes < 0)
+        {
+            System.out.println( "Invalid Value");
+            return;
+        }
+        long year = ((minutes /60) /24) /365;
+        long day = ((minutes /60) /24) % 365;
+        System.out.println(minutes + " min = " + year + " y and " + day + " d");
+
     }
 }
